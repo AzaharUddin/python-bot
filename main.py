e33004ae0f4a3f67714a11f622d5d8ebb6b236f5
AzaharUddin <azahar.mallick399@gmail.com>
2024-08-21 07:12:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ TOKEN = "TRON"
 
 BOT_TOKEN = "6904010121:AAEiZqkK_FoXI-DK9g1v2iiaurRjnR-IO8A"
 PAYMENT_CHANNEL = "@testpostchnl" #add payment channel here including the '@' sign
-OWNER_ID = 5151868182 #write owner's user id here.. get it from @MissRose_Bot by /id
+OWNER_ID = 1870276025 #write owner's user id here.. get it from @MissRose_Bot by /id
 CHANNELS = ["@testpostchnl"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
               #you can add as many channels here and also add the '@' sign before channel username
 Daily_bonus = 1 #Put daily bonus amount here!
