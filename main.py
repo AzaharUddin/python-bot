73edbf0f9e110ff599abe7060da8534ac5364fd9
AzaharUddin <azahar.mallick399@gmail.com>
2024-08-21 13:57:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import telebot
 ##TOKEN DETAILS
 TOKEN = "TRON"
 
-BOT_TOKEN = "6904010121:AAEiZqkK_FoXI-DK9g1v2iiaurRjnR-IO8A"
+BOT_TOKEN = "7405834753:AAGRBgIdGU6PCjygm9UAHXfEG7pZExWHTe0"
 PAYMENT_CHANNEL = "@ReferAndEarn399" #add payment channel here including the '@' sign
 OWNER_ID = 1870276025 #write owner's user id here.. get it from @MissRose_Bot by /id
 CHANNELS = ["@ReferAndEarn399"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
